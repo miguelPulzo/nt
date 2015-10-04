d5cf93b1c5ae0d7905d3ec210b0319b78f9af650
Miguel Angel G.R <miguel.garcia@pulzo.com>
2015-10-03 21:54:03 -0500

    [master] Proyecto Drupal.

---
+++ sites/all/themes/contrib/aurora/config.rb
@@ -0,0 +1,36 @@
+# Require any additional compass plugins here.
+require 'singularitygs'
+require 'toolkit'
+
+# Set this to the root of your project when deployed:
+http_path = "/sites/all/themes/contrib/aurora"
+css_dir = "css"
+sass_dir = "sass"
+images_dir = "images"
+javascripts_dir = "js"
+
+# You can select your preferred output style here (can be overridden via the command line):
+output_style = :compressed
+
+# To enable relative paths to assets via compass helper functions. Uncomment:
+relative_assets = true
+
+# To disable debugging comments that display the original location of your selectors. Uncomment:
+# line_comments = false
+
+
+# If you prefer the indented syntax, you might want to regenerate this
+# project again passing --syntax sass, or you can uncomment this:
+# preferred_syntax = :sass
+# and then run:
+# sass-convert -R --from scss --to sass sass scss && rm -rf sass && mv scss sass
+
+
+
+
+source 'https://rubygems.org'
+
+gem 'toolkit', '~&gt;1.0.0'
+gem 'singularitygs', '~&gt;1.0.7'
+gem 'breakpoint', '~&gt;2.0.2'
+gem 'sassy-buttons', '~&gt;0.1.4'
